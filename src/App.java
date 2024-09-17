7e693b43f14c3105d93f447c82fde1120daf48a5
Jaime Zafra <jzr00004@red.ujaen.es>
2024-09-17 09:56:05 +0200

    problem set added

--- src/App.java
+++ src/App.java
@@ -1,9 +1,5 @@
 public class App {
+	public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws Exception {
-        Problem problem = new Problem(args[0]);
-        Greedy solver = new Greedy();
-        Algorithm.Solution solution = solver.Solve(problem);
-        System.out.println(solution.value);
-    }
+	}
 }
