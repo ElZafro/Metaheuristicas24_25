db030df235eab306375dfb880fc19f05e5630422
Jaime <r0984423@student.thomasmore.be>
2024-09-25 22:19:33 +0200

    Implementación del archivo config.txt para la asignación de constantes

--- src/EjecucionPractica.java
+++ src/EjecucionPractica.java
@@ -1,24 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class EjecucionPractica {
+    /*
     private static int LIMIT = 5;
 
     private static long SEMILLA = 77688090;
 
     private final String[] nombresArchivos = {"a280.tsp", "ch130.tsp", "d18512.tsp", "pr144.tsp", "u1060.tsp"};
+     */
 
     public EjecucionPractica() {}
 
     public void exec(){
-        for(int i = 0; i < nombresArchivos.length; ++i){
-            AlmacenarDatos.inicializacionDatos(nombresArchivos[i]);
-            ArrayList<Integer> solucion = GreedyAleatorio.greedy(AlmacenarDatos.getDistancias().length, AlmacenarDatos.getDistancias(), LIMIT, SEMILLA);
+        int LIMIT = 0;
+        long SEMILLA = 0;
+        try (FileReader fr = new FileReader("config.txt")) {
+            BufferedReader br = new BufferedReader(fr);
+            //Para el LIMIT
+            String linea = br.readLine();
+            LIMIT = Integer.parseInt(linea.split(" ")[2]);
+            //Para la SEMILLA
+            linea = br.readLine();
+            SEMILLA = Long.parseLong(linea.split(" ")[2]);
+            //Para el archivo
+            linea = br.readLine();
+            String nombreArchivo = linea.split(" ")[2];
+            AlmacenarDatos.inicializacionDatos(nombreArchivo);
+
+            //Para el algoritmo
+            linea = br.readLine();
+            long startTime = 0, endTime = 0;
+            ArrayList<Integer> solucion = new ArrayList<>();
+            if(linea.split(" ")[2].equals("GreedyAleatorio")){
+                startTime = System.nanoTime();
+                solucion = GreedyAleatorio.greedy(AlmacenarDatos.getDistancias().length, AlmacenarDatos.getDistancias(), LIMIT, SEMILLA);
+                endTime = System.nanoTime();
+            }
+            long duration = endTime - startTime;
+
             double costeTotal = 0;
             for(int j = 0; j < (solucion.size() - 1); ++j){
                 costeTotal += AlmacenarDatos.getDistancias()[solucion.get(j)][solucion.get(j + 1)];
             }
-            System.out.println("Problema " + nombresArchivos[i] + ": coste de " + costeTotal);
+            System.out.println("Problema " + nombreArchivo + ":\nCoste de " + costeTotal);
+            System.out.println("Tiempo de ejecución: " + (duration/1_000_000.0) + " milisegundos");
             System.out.println("Solucion: " + solucion);
+        } catch(IOException e){
+            e.printStackTrace();
         }
     }
 }
